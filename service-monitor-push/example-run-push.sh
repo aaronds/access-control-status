35aae7c181b1b45eeb228f0bf33b04a64f8b7127
Aaron Shrimpton <aaronds@gmail.com>
2025-08-01 17:40:37 +0100

    Example push script

---
+++ service-monitor-push/example-run-push.sh
@@ -0,0 +1,9 @@
+export SQS_POWER_URL="https://sqs.eu-west-2.amazonaws.com/123/pro-power"
+export SQS_MODE_URL="https://sqs.eu-west-2.amazonaws.com/123/pro-mode"
+export webClientRole=arn:aws:iam::123:role/ROLE
+
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+export AWS_SESSION_TOKEN=""
+
+node index.mjs
